799e253dfd2260f51a1af8be14fcd17d4e3d4294
Andrew Siemer <andrew@siemer.org>
2019-10-22 15:01:39 -0500

    [refactor] used alias definitions

--- definitions.h
+++ definitions.h
@@ -1,5 +1,5 @@
-// Sonar Project - Analog Electronics (Fall 2019)
-// main.c
+// definitions.h
+// @description A convenient place for all program definitions.
 // 
 // @author Andrew Siemer <andrew.siemer@eagles.oc.edu>
 // @version 10.19.19
@@ -10,22 +10,12 @@
 
 /**
  * constants
- *  -- NOTICE - DO NOT END THESE STATEMENTS WITH SEMICOLONS
  */
 #define ON 1
 #define OFF 0
 #define LOW 0
 #define HIGH 1
 
-#define LCD_RS _LATF4
-#define LCD_RW _LATF6
-#define LCD_EN _LATF5
-#define LCD_DB4 _LATB9
-#define LCD_DB5 _LATB8
-#define LCD_DB6 _LATB7
-#define LCD_DB7 _LATB6
-
-/*
 #define LCD_RS digOutput22  //_LATF4
 #define LCD_RW digOutput16  //_LATF6
 #define LCD_EN digOutput21  //_LATF5
@@ -33,7 +23,6 @@
 #define LCD_DB5 digOutput24 //_LATB8
 #define LCD_DB6 digOutput25 //_LATB7
 #define LCD_DB7 digOutput26 //_LATB6
-*/
 
 /*
  * function definitions
