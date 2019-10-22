35e2a245a3ac329fc7dab52b8de8e3adece79cc6
Andrew Siemer <andrew@siemer.org>
2019-10-22 15:02:19 -0500

    [test] multi-line LCD output

--- main.c
+++ main.c
@@ -1,43 +1,54 @@
-// Sonar Project - Analog Electronics (Fall 2019)
 // main.c
+// @description Sonar Project - Analog Electronics (Fall 2019)
 // 
 // @author Andrew Siemer <andrew.siemer@eagles.oc.edu>
-// @version 10.16.19
+// @version 10.22.19
 //
 
 #include <p30f3013.h>
 #include "aliases.h"
 #include "utilities.h"
 #include "definitions.h"
-
-#include "lcd.h"
+/*
+#include <stdio.h> //to resolve printf error
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libpic30.h>
+*/
 
 int main() {
-    initializeUART();
-    setupPins();
-    LCD_Init();
-    pause(1000);
-    printf("I'm in the main!\n");
-    char const * TEXT2 = "Hello World!";
-    LCD_Write_XY(1, 0, TEXT2);
-    
-    while(1) {
-      ; //Infinite loop
-    }
-    halt();
-    return (0);
+  initializeUART();
+  setupPins();
+  LCD_Init();
+  pause(2000);
+  printf("Writing to display...\n");
+
+  //LCD_Clear();
+  char const * TEXT = "hello world";
+  LCD_Write_XY(1, 0, TEXT);
+  TEXT = "line 2";
+  LCD_Write_XY(2, 0, TEXT);
+  TEXT = "line 3 ";
+  LCD_Write_XY(3, 0, TEXT);
+  TEXT = "line 4";
+  LCD_Write_XY(4, 0, TEXT);
+ 
+  halt();
+  return (0);
 }
 
 void setupPins() {
-    pin23Type = DIGITAL; 
-    pin24Type = DIGITAL; 
-    pin25Type = DIGITAL; 
-    pin26Type = DIGITAL;
-    pin16Direction = OUTPUT;
-    pin21Direction = OUTPUT; 
-    pin22Direction = OUTPUT; 
-    pin23Direction = OUTPUT; 
-    pin24Direction = OUTPUT; 
-    pin25Direction = OUTPUT; 
-    pin26Direction = OUTPUT; 
+  pin23Type = DIGITAL; 
+  pin24Type = DIGITAL; 
+  pin25Type = DIGITAL; 
+  pin26Type = DIGITAL;
+  pin16Direction = OUTPUT;
+  pin21Direction = OUTPUT; 
+  pin22Direction = OUTPUT; 
+  pin23Direction = OUTPUT; 
+  pin24Direction = OUTPUT; 
+  pin25Direction = OUTPUT; 
+  pin26Direction = OUTPUT; 
 }
